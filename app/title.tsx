6973bc1816ae6e713388e0859990d7865d416027
Abrar-eng <54318551+Abrar-eng@users.noreply.github.com>
2024-10-17 02:48:08 +0600

    Title File Created

---
+++ app/title.tsx
@@ -0,0 +1,8 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: {
+      template: '%s | Documentation',
+      default: 'Documentation', // a default is required when creating a template
+    },
+  }
